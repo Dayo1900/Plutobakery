c33a2fd0c4c365e85cc867754b25011684c59707
Dayo1900 <73638760+Dayo1900@users.noreply.github.com>
2021-11-09 17:45:00 +0100

    Create server_payment.php

---
+++ server_payment.php
@@ -0,0 +1,43 @@
+
+<?php
+
+
+// initiate connection with database
+
+
+//connect to database
+$conn = mysqli_connect('localhost', 'id17048003_gahs', 'Temitope.1900', 'id17048003_customer');
+
+//check connection
+if (!$conn) 
+{ 
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+
+$fname=$sname=$prefix="";   //initialise session variables and set to zero
+
+$fname=$_SESSION['fname'];
+$sname=$_SESSION['sname'];
+
+
+
+
+//insert data into database
+$submit_1 = "INSERT INTO bread_orders (fname, sname, phone, bread_type, quantity, price) 
+VALUES ('$fname', '$sname', '$phone', '$bread_type', '$quantity', '$price')";
+
+
+if( !mysqli_query($conn, $submit_1))
+{
+echo "<p class='d' style='text-align:center;'>Unable to receive payment at the moment: </p>" . mysqli_error($conn);
+}
+else
+{
+    //redirect to payment page to be handled by Interswitch
+    echo "<meta http-equiv='refresh' content='2;url=payment.php' />";
+}
+
+mysqli_close($conn);
+
+?>
